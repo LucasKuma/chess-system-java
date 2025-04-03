51a92895a698bd65a7157eef330d6e834f6be0d9
Lucas Kuhlmann <lucas.khmn@gmail.com>
2025-04-03 17:09:34 -0300

    Special move - Castling PART 1

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -20,7 +20,8 @@ public class Knight extends ChessPiece {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p == null || p.getColor() != getColor();
 	}
-
+	
+	
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
